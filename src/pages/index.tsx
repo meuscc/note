57f7886c327f86c1ccb2db1fb5acd434bdb066d9
shihai5 <shihai@pandu.ie>
2022-03-28 03:46:42 +0800

    u

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,10 @@
-import React, {useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 
 import Tet from "../contents/langs/cpp/cpp-cheat/cpp-cheat.mdx";
 
 export const HomePage: NextPage = () => {
-  const socketRef = useRef<WebSocket>()
-  useEffect(() => {
-
-    let socket = new WebSocket("ws://localhost:8181");
-    socket.onopen = function () { console.log("success");    };
-    socket.onmessage = function (msg) { console.log(msg.data); };
-    socket.onclose = function () { console.log("closed"); };
-    socketRef.current = socket
-
-
-  }, [])
-
   return (
     <div>
       <Head>
@@ -24,14 +12,11 @@ export const HomePage: NextPage = () => {
         <meta name="description" content="c++ 简要笔记" />
       </Head>
       <div className={"book-page article heti heti--sans"}>
-        <button onClick={() => {
+        <button onClick={() => {}} className={"btn-success btn-fill"}>
+          发送
+        </button>
 
-          socketRef.current?.send(`${Math.random()}`)
-        }} className={'btn-success btn-fill'}>发送</button>
-
-        <div>
-          <m-b>f(x) x^2</m-b>
-        </div>
+        <div></div>
         <Tet />
       </div>
     </div>
