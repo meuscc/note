16a631ea563ed2d123458f4672f11d1277755220
shihai5 <shihai@pandu.ie>
2022-04-22 19:28:46 +0800

    update

--- src/pages/articles/articles.tsx
+++ src/pages/articles/articles.tsx
@@ -1,9 +1,55 @@
 export default function Articles() {
   return (
     <div>
-      <h1>心态</h1>
-      <h5>希望</h5>
-      <h5>欢乐</h5>
+      <div>
+        <h5>无生</h5>
+        <h5>无死</h5>
+        <h5>无知</h5>
+      </div>
+      <div>
+        <h5>天真</h5>
+        <h5>欢怡</h5>
+        <h5>希望</h5>
+      </div>
+      <div>
+        <h5>健康</h5>
+        <h5>财富</h5>
+        <h5>享乐</h5>
+      </div>
+      <div>
+        <h5>商业: 人群(社交网络) 信息(知识获取) 能量(资产积累)</h5>
+        <h5>金融: 总有规律</h5>
+      </div>
+      <div>
+        <p>我要知道</p>
+        <h5>数学</h5>
+        <h5>物理</h5>
+        <h5>计算机</h5>
+      </div>
+      <div>
+        <p>当前计划</p>
+        <h5>游戏脚本(c#, python, c++)</h5>
+        <h5>扑克游戏(cocos, go)</h5>
+        <h5>联络软件(flutter, go)</h5>
+        <h5>咨询分享(flutter, go)</h5>
+        <h5>太空激战(godot)</h5>
+        <h5>PS AI Blender</h5>
+      </div>
+      <div>
+        <p>长远计划</p>
+        <div>天真数学笔记</div>
+        <div>天真物理笔记</div>
+        <div>天真计算笔记</div>
+        <h5>计算机智能交易(c++, rust, julia)</h5>
+        <h5>情感意识模拟(c++, rust, julia)</h5>
+        <h5>梦中家园建设(unreal, c++, rust, julia)</h5>
+        <h5>自动驾驶(unreal, c++, rust, julia)</h5>
+      </div>
+      <div>
+        <p>终生计划</p>
+        <h5>数学学习而已</h5>
+        <h5>物理学习而已</h5>
+      </div>
     </div>
   );
 }
