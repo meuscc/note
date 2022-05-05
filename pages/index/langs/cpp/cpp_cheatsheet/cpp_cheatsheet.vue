5840169414ae7a43086af64d3f89698928b98c5e
shihai5 <shihai@pandu.ie>
2022-05-05 12:26:53 +0800

    u

--- pages/index/langs/cpp/cpp_cheatsheet/cpp_cheatsheet.vue
+++ pages/index/langs/cpp/cpp_cheatsheet/cpp_cheatsheet.vue
@@ -2,7 +2,8 @@
   <h1>Cpp sdsdsdf s</h1>
   <h1>Cpp sdsdsdf s</h1>
 
-  <router-link to="/langs/rust/rust_cheatsheet/rust_cheatsheet"
-    >去 Rust</router-link
-  >
+  <router-link to="/langs/rust/rust_cheatsheet/rust_cheatsheet">
+    去 Rustdsf sfsf 士大夫士大夫是的發是的發
+  </router-link>
 </template>
+<script setup></script>
