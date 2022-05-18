7302557a89fb7e918ec2b8bf509b17ce06ba23b8
shihai5 <shihai@pandu.ie>
2022-05-18 17:44:55 +0800

    u

---
+++ .nuxt/types/content.d.ts
@@ -0,0 +1,4 @@
+declare module '#content/server' {
+  const serverQueryContent: typeof import('D:/Wss/meuscc/notes/node_modules/@nuxt/content/dist/runtime/server').serverQueryContent
+  const parseContent: typeof import('D:/Wss/meuscc/notes/node_modules/@nuxt/content/dist/runtime/server').parseContent
+}
