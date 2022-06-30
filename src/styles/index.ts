0db9738da5664c5dd8998f94ebe5833319e44706
shihai5 <shihai@pandu.ie>
2022-06-30 19:25:27 +0800

    u

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,8 @@
 import createThemes from "/src/themes/create-themes";
 import createReboot from "/src/styles/reboot";
+import * as themes from "/src/styles/themes";
+
+console.log(themes);
 
 // @ts-ignore
 document.adoptedStyleSheets = [
