1a91917795e57620f86c735616101b49244bed4c
shihai5 <shihai@pandu.ie>
2022-07-20 02:53:59 +0800

    u

--- src/components/tree.ts
+++ src/components/tree.ts
@@ -11,20 +11,42 @@ export class Tree extends LitElement {
       :host {
         display: block;
       }
+      .level-control.level-1 {
+        /*background-color: rgba(0, 0, 0, 0.1);*/
+      }
+      .level-control.level-2 {
+        /*background-color: pink;*/
+      }
+      .level-control.level-3 {
+        /*background-color: cyan;*/
+      }
+      .level-control.level-4 {
+        /*background-color: coral;*/
+      }
+      .level-control.level-5 {
+        /*background-color: deeppink;*/
+      }
     `,
   ];
 
   @property()
   data: TreeData = navs;
 
+  @property()
+  level: number = 1;
+
   render() {
-    console.log(this.data);
     return html`
-      <y-a href="${this.data.link}">${this.data.name}</y-a>
-      ${map(
-        this.data.children,
-        (child) => html`<y-tree .data="${child}"></y-tree>`
-      )}
+      <div class="level-item level-${this.level}">
+        <div class="level-control level-${this.level}">
+          <y-a href="${this.data.link}">${this.data.name}</y-a>
+        </div>
+        ${map(
+          this.data.children,
+          (child) =>
+            html`<y-tree level="${Number(this.level) + 1}" .data="${child}" />`
+        )}
+      </div>
     `;
   }
 }
